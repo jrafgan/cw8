f5618f72e09e5f40bfb52250b86925ce1ac50e7d
Maksat Duishenaliev <maksatovy@gmail.com>
2019-02-02 14:12:56 +0600

    new version added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route, Switch} from "react-router";
+import {Container} from "reactstrap";
+
+import Header from "./components/Header/Header";
+import AddPost from "./containers/AddPost/AddPost";
+
 import './App.css';
+import PostList from "./containers/PostList/PostList";
+import FullPost from "./components/Post/FullPost";
+import EditPost from "./containers/EditPost/EditPost";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+          <Container>
+              <Switch>
+                  <Route path="/" exact component={PostList}/>
+                  <Route path="/posts/add" exact render={(props) => <AddPost {...props} />}/>
+                  <Route path="/posts/:id" exact render={(props) => <FullPost {...props}/>}/>
+                  <Route path="/posts/:id/edit"  render={(props) => <EditPost {...props} />}/>
+                  <Route path="/about" exact render={() => <h1>About</h1>}/>
+                  <Route path="/contact" exact component={FullPost}/>
+              </Switch>
+
+          </Container>
       </div>
     );
   }
